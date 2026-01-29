b932268dd495bec08e4aa324b4364d438ba2e788
mathis <mathisfloch29@gmx.fr>
2026-01-29 10:09:31 +0100

    first commit

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configuration de l'application
-const config = {
-    apiKey: "AKIAIOSFODNN7EXAMPLE",
-    secretkey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
-    dbPassword: "super_secret_password_123",
-    githubToken: "ghp_xxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-};
-module.exports = config;
