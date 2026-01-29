082645303235b090bd18b6b5406c46772bc5e5d9
mathis <mathisfloch29@gmx.fr>
2026-01-29 09:47:49 +0100

    Add config

---
+++ config.js
@@ -0,0 +1,8 @@
+// Configuration de l'application
+const config = {
+    apiKey: "AKIAIOSFODNN7EXAMPLE",
+    secretkey: "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY",
+    dbPassword: "super_secret_password_123",
+    githubToken: "ghp_xxxxxxxXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+};
+module.exports = config;
